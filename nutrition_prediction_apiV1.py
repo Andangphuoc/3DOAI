8e48448dc5ca51888e2af787fd727fa67d42acad
Andp <117456949+Andangphuoc@users.noreply.github.com>
2025-07-08 13:55:58 +0700

    Update nutrition_prediction_apiV1.py

--- nutrition_prediction_apiV1.py
+++ nutrition_prediction_apiV1.py
@@ -308,5 +308,6 @@ async def root():
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.environ.get("PORT", 8000))
+
+    port = int(os.environ.get("PORT", 8000))  # Lấy PORT từ biến môi trường
     uvicorn.run(app, host="0.0.0.0", port=port)
